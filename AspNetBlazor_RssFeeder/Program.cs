969db804e326ec081219877ad952a9315dc63531
Egor Vorontsov <egor20vv@gmail.com>
2022-09-21 19:36:00 +0700

    Revert "Settings service was added"
    This reverts commit b1b38d61199d0b603d66e9bc433182a51a9342d0.

--- AspNetBlazor_RssFeeder/Program.cs
+++ AspNetBlazor_RssFeeder/Program.cs
@@ -1,14 +1,7 @@
-using AspNetBlazor_RssFeeder.Services;
-using AspNetBlazor_RssFeeder.Services.Interfaces;
-
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// settings service
-builder.Services.AddSingleton<ISettingsService, SettingsService>(_ => SettingsService.CreateWithFile());
-
 
 // Add services to the container.
 builder.Services.AddRazorPages();
